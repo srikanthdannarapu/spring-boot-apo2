62e55ec1359d20c118edb6c3b9b2d51a8c79df33
Srikanth Dannarapu <srikant.dannarapu@gmail.com>
2020-09-04 12:09:32 +0530

    1st commit

---
+++ src/main/java/com/srikanth/aop2/SpringBootApo2Application.java
@@ -0,0 +1,13 @@
+package com.srikanth.aop2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootApo2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/srikanth/aop2/SpringBootApo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.srikanth.aop2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootApo2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
